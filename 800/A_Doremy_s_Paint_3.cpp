002ccda13080befba5e67e33e43a6edd1225f347
gauravkumar1364 <gaurav11gkk@gmail.com>
2025-10-15 20:20:32 +0530

    new

--- 800/A_Doremy_s_Paint_3.cpp
+++ 800/A_Doremy_s_Paint_3.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define all(x) (x).begin(), (x).end()
-#define endl '\n'
-
-string solve(int n, vector<int>& arr) {
-    int k = arr[0] + arr[1];
-    unordered_map<int,int> mp;
-    mp.push(arr[0],)
-    for (int i = 2; i < n - 1; i++) {
-        if (arr[i] + arr[i + 1] != k)
-            return "No";
-    }
-    return "Yes";
-}
-
-int32_t main() { 
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
+int main()
+{
+    int t; // Number of test cases
     cin >> t;
-    while (t--) {
-        int n;
+    while (t--)
+    {
+        long long n; // Length of the array for the current test case
         cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        cout << solve(n, a) << endl;  
+        long long a[n]; // Array to store the elements
+        for (int i = 0; i < n; i++) // Input the elements of the array
+            cin >> a[i];
+        
+        // Map to store the frequency of each element in the array
+        map<long long, long long> frequency_map;
+        for (int i = 0; i < n; i++) // Calculate the frequency of each element
+            frequency_map[a[i]]++;
+        
+        // Check the number of distinct elements in the array
+        if (frequency_map.size() >= 3) // If there are 3 or more distinct elements
+        {
+            cout << "No" << endl; // It's not possible to make the array good
+        }
+        else
+        {
+            // Get the frequency of the smallest and largest elements
+            long long freq_1 = frequency_map.begin()->second;
+            long long freq_2 = frequency_map.rbegin()->second;
+            
+            // Check if the array can be made good
+            if (freq_1 == freq_2) // If both frequencies are equal
+                cout << "Yes" << endl; // The array can be made good
+            else if (n % 2 == 1 && abs(freq_1 - freq_2) == 1) // If the array length is odd and the frequency difference is 1
+                cout << "Yes" << endl; // The array can be made good
+            else
+                cout << "No" << endl; // Otherwise, it's not possible
+        }
     }
-
     return 0;
 }
+
+// Time Complexity (TC): O(nlog2n) = O(100*log2(100)) = O(100*7) = O(700)
+// Space Complexity (SC): O(n+n) = O(2n) = O(n) = O(100)
