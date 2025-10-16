c4ba7ca7366ee24b1a228c26c0dfe60cc0704213
gauravkumar1364 <gaurav11gkk@gmail.com>
2025-10-16 12:42:46 +0530

    Update C_Vasilije_in_Cacak.cpp

--- 900/C_Vasilije_in_Cacak.cpp
+++ 900/C_Vasilije_in_Cacak.cpp
@@ -2,17 +2,27 @@
 using namespace std;
 
 
-string solve(int n,int k, int x) {
+string solve(long long n,long long k, long long x) {
+    long long minsum = (k*(k+1))/2;
+    long long maxsum = (n*(n+1))/2 - ((n-k)*(n-k+1))/2;
+    
+    if(x<=maxsum && minsum<=x) return "YES";
+    else return "NO";
     
 }
-
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t = 1;
-    // cin >> t; // Uncomment if multiple test cases
-    while (t--) solve();
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k, x;
+        cin >> n >> k >> x;
+
+        cout << solve(n, k, x) << "\n";
+    }
 
     return 0;
 }
